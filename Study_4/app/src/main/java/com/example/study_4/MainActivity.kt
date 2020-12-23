8ae1f5bb1671ea64a4ebd6be7e8029b6cdede7d8
haelin_kim <haelin_kim@naver.com>
2020-12-23 22:30:52 +0900

    12월 23일 커밋

--- Study_4/app/src/main/java/com/example/study_4/MainActivity.kt
+++ Study_4/app/src/main/java/com/example/study_4/MainActivity.kt
@@ -2,68 +2,67 @@ package com.example.study_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.study_4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var new = "0"
         var old = "0"
 
-        btn1.setOnClickListener {
+        binding.btn1.setOnClickListener {
             new += "1"
-            result.text = new
+            binding.result.text = new
         }
-        btn2.setOnClickListener {
+        binding.btn2.setOnClickListener {
             new += "2"
-            result.text = new
+            binding.result.text = new
         }
-        btn3.setOnClickListener {
+        binding.btn3.setOnClickListener {
             new += "3"
-            result.text = new
+            binding.result.text = new
         }
-        btn4.setOnClickListener {
+        binding.btn4.setOnClickListener {
             new += "4"
-            result.text = new
+            binding.result.text = new
         }
-        btn5.setOnClickListener {
+        binding.btn5.setOnClickListener {
             new += "5"
-            result.text = new
+            binding.result.text = new
         }
-        btn6.setOnClickListener {
+        binding.btn6.setOnClickListener {
             new += "6"
-            result.text = new
+            binding.result.text = new
         }
-        btn7.setOnClickListener {
+        binding.btn7.setOnClickListener {
             new += "7"
-            result.text = new
+            binding.result.text = new
         }
-        btn8.setOnClickListener {
+        binding.btn8.setOnClickListener {
             new += "8"
-            result.text = new
+            binding.result.text = new
         }
-        btn9.setOnClickListener {
+        binding.btn9.setOnClickListener {
             new += "9"
-            result.text = new
+            binding.result.text = new
         }
-        btn0.setOnClickListener {
+        binding.btn0.setOnClickListener {
             new += "0"
-            result.text = new
+            binding.result.text = new
         }
 
-        btnP.setOnClickListener {
+        binding.btnP.setOnClickListener {
             old= (old.toInt() + new.toInt()).toString()
             new="0"
-            result.text=old
+            binding.result.text=old
         }
 
-        btnE.setOnClickListener {
+        binding.btnE.setOnClickListener {
             new="0"
             old="0"
-            result.text="0"
+            binding.result.text="0"
         }
 
     }
