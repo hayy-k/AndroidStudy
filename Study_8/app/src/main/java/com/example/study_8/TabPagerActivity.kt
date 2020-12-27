b69ee87f78b4f921764fb3527fc9104dc6901f1f
haelin_kim <haelin_kim@naver.com>
2020-12-27 22:08:29 +0900

    12월 27일 커밋

---
+++ Study_8/app/src/main/java/com/example/study_8/TabPagerActivity.kt
@@ -0,0 +1,67 @@
+package com.example.study_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_tab_pager.*
+
+class TabPagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_pager)
+
+        tab_layout.addTab(tab_layout.newTab().setText("ONE"))
+        tab_layout.addTab(tab_layout.newTab().setText("TWO"))
+        tab_layout.addTab(tab_layout.newTab().setText("THREE"))
+
+        val pagerAdapter = PagerAdapter(supportFragmentManager, 3)
+        view_pager.adapter = pagerAdapter
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                view_pager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+    }
+}
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount:Int
+):FragmentStatePagerAdapter(fragmentManager){
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0->{
+                return Fragment1()
+            }
+            1->{
+                return Fragment2()
+            }
+            2->{
+                return Fragment3()
+            }
+            else->{
+                return Fragment1()
+            }
+        }
+    }
+}
