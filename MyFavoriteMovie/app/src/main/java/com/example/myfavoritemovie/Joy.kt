f5facf3a52f754052ea9ba9dd53acc7de28d714b
haelin_kim <haelin_kim@naver.com>
2020-12-29 23:29:25 +0900

    12월 29일 커밋

---
+++ MyFavoriteMovie/app/src/main/java/com/example/myfavoritemovie/Joy.kt
@@ -0,0 +1,11 @@
+package com.example.myfavoritemovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Joy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joy)
+    }
+}
---
+++ MyFavoriteMovie/app/src/main/java/com/example/myfavoritemovie/Sadness.kt
@@ -0,0 +1,11 @@
+package com.example.myfavoritemovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sadness : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sadness)
+    }
+}
