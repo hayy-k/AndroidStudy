e2947f1f3993c0e6dfd2bff0cb8275fac8946b60
haelin_kim <haelin_kim@naver.com>
2021-01-03 21:38:07 +0900

    1월 3일 커밋

--- Study_8/app/src/main/java/com/example/study_8/MainActivity2.kt
+++ Study_8/app/src/main/java/com/example/study_8/MainActivity2.kt
@@ -2,35 +2,28 @@ package com.example.study_8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 
-class ClassStudy : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_class_study)
+        setContentView(R.layout.activity_main2)
+        Car("v8 engine", "big")
+        val supercar: SuperCar = SuperCar("good", "big", "white")
+
+        val runableCar:RunableCar= RunableCar("simple engine", "simple body")
+        runableCar.ride()
+        runableCar.run()
+        runableCar.navi("서울")
+
+        val runableCar2:RunableCar2= RunableCar2("engine2", "body2", "door2")
+        Log.d("runnableCar2",""+runableCar2.engine)
+        Log.d("runnableCar2",""+runableCar2.body)
+
     }
 }
 
-fun main(array:Array<String>){
-    Car("v8 engine","big")
-    val bigCar = Car("v7 engine", "big")
-    val bigCar1 :Car = Car("v7 engine", "big")
-    SuperCar("good", "big", "white")
-
-    val runableCar:RunableCar= RunableCar("simple engine","simple body")
-
-    runableCar.ride()
-    runableCar.run()
-    runableCar.navi("서울")
-
-    val runableCar2:RunableCar2= RunableCar2("engine2","body2","door2")
-    println(runableCar2.engine)
-    println(runableCar2.body)
-}
-
-class Car(var engine: String, var body: String){
-}
-
-/*
 class SuperCar {
     var engine: String
     var body: String
@@ -45,9 +38,9 @@ class SuperCar {
 
 class Car1(engine: String, body: String)
 {
-    var door: String  = ""
+    var door: String = ""
 
-    constructor(engine: String, body: String, door: String) : this(engine, body)
+    constructor(engine: String, body: String, double: String) : this(engine, body)
     {
         this.door = door
     }
@@ -77,7 +70,6 @@ class RunableCar(engine: String, body: String) {
 
     fun run() {
         println("달립니다.")
-
     }
 
     fun navi(destination: String) {
@@ -90,10 +82,13 @@ class RunableCar2{
     var body:String
     var door:String
 
-    constructor(Engine: String,Body: String,Door: String){
+    constructor(Engine: String, Body: String, Door: String) {
         this.engine=Engine
         this.body=Body
         this.door=Door
     }
+
+    init {
+        Log.d("Runnable2가 만들어졌습니다.","runnable22")
+    }
 }
-*/
