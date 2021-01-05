f96d3a56313d03bcc32d24e190813c6f0dce4ea0
haelin_kim <haelin_kim@naver.com>
2021-01-05 19:23:42 +0900

    1월 5일 커밋

---
+++ DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/SplashActivity.kt
@@ -0,0 +1,4 @@
+package com.example.diaryinmyhand
+
+class SplashActivity {
+}
---
+++ ToDoList/app/src/main/java/com/example/todolist/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.SystemClock
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SystemClock.sleep(300)
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
