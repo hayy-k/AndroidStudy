c9eef485e8e7c2b928169c1a4e181de7118d35f3
haelin_kim <haelin_kim@naver.com>
2021-01-12 22:35:30 +0900

    1월 12월 커밋

--- MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPAN_COUNT = 2
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
