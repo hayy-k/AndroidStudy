7f07b39fab51b5f42afcc4a5ce5e05053626a46f
haelin_kim <haelin_kim@naver.com>
2021-01-20 23:15:21 +0900

    1월 20일 커밋

--- Study_5/app/src/main/java/com/example/study_5/Intent1.kt
+++ Study_5/app/src/main/java/com/example/study_5/Intent1.kt
@@ -3,20 +3,23 @@ package com.example.study_5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_intent1.*
+import com.example.study_5.databinding.ActivityIntent1Binding
 
 class Intent1 : AppCompatActivity() {
+    private lateinit var binding : ActivityIntent1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intent1)
+        binding = ActivityIntent1Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         var acb = 10
         var b= acb *10
         var c= acb *100
 
-        change_activity.setOnClickListener {
+       /* binding.change_activity.setOnClickListener {
             val intent=Intent(this@Intent1,Intent2::class.java)
             startActivity(intent)
-        }
+        }*/
     }
 }
