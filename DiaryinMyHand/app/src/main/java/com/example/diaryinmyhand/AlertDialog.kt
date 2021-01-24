f7c30c8071a5ebc3ea1748f31ba3f95e86167562
haelin_kim <haelin_kim@naver.com>
2021-01-24 23:49:24 +0900

    1월 24일 커밋

--- DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/AlertDialog.kt
+++ DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/AlertDialog.kt
@@ -1,8 +1,12 @@
 package com.example.diaryinmyhand
 
 import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.diaryinmyhand.R
 import com.example.diaryinmyhand.databinding.ActivityAlertDialogBinding
 
 class AlertDialog : AppCompatActivity() {
@@ -13,9 +17,61 @@ class AlertDialog : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-     /*   binding.more.setOnClickListener {
+        /*binding.Back.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this)
+                    .setTitle("경고")
+                    .setMessage("내용을 저장하지 않고 나가시겠습니까?")
+                    .setPositiveButton("네", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "일기 입력을 종료합니다.", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                    })
+                    .setNegativeButton("아니요", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+                    })
+                    .create()
+                    .show()
+        }*/
+
+        binding.more.setOnClickListener {
             val alertDialog = AlertDialog.Builder(this)
                     .setTitle("목록")
+                    .setMessage("수행할 내용을 알려주시용!")
+                    .setPositiveButton("수정", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "일기를 수정합니다.", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, DiaryWriting::class.java)
+                        startActivity(intent)
+                    })
+                    .setNegativeButton("삭제", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "일기를 삭제합니다.", Toast.LENGTH_SHORT).show()
+                        //일기 삭제하는 기능...
+                    })
+                    .setNeutralButton("이모티콘", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "이모티콘을 설정합니다.", Toast.LENGTH_SHORT).show()
+                        //이모티콘 어쩌지.....
+                    })
+                    .create()
+                    .show()
+        }
+
+
+
+
+        /*binding.more.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this)
+                    .setTitle("목록")
+                    .setItems(R.array.data_name, DialogInterface.OnClickListener { dialog, which ->
+                        when(which){
+
+                            0 -> binding.more.setText("수정")
+                            0 -> DialogInterface.OnCancelListener {
+                            val intent = Intent(this, DiaryWriting::class.java)
+                        }
+                            1 -> binding.more.setText("삭제")
+                            2 -> binding.more.setText("이모티콘")
+
+                        }
+                    })
         }*/
 
 
