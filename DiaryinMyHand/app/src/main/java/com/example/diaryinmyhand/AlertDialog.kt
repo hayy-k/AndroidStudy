3c1079a74a6d54c8a54fc17536d2bb7540115ff2
haelin_kim <haelin_kim@naver.com>
2021-01-25 19:07:03 +0900

    1월 25일 커밋

--- DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/AlertDialog.kt
+++ DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/AlertDialog.kt
@@ -16,22 +16,7 @@ class AlertDialog : AppCompatActivity() {
         binding = ActivityAlertDialogBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        /*binding.Back.setOnClickListener {
-            val alertDialog = AlertDialog.Builder(this)
-                    .setTitle("경고")
-                    .setMessage("내용을 저장하지 않고 나가시겠습니까?")
-                    .setPositiveButton("네", DialogInterface.OnClickListener { dialog, which ->
-                        Toast.makeText(this, "일기 입력을 종료합니다.", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, MainActivity2::class.java)
-                        startActivity(intent)
-                    })
-                    .setNegativeButton("아니요", DialogInterface.OnClickListener { dialog, which ->
-                        Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_SHORT).show()
-                    })
-                    .create()
-                    .show()
-        }*/
+        
 
         binding.more.setOnClickListener {
             val alertDialog = AlertDialog.Builder(this)
